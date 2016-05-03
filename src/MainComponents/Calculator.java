7670d9911b01d285ab7bbc5ead5ae06e1e5103cd
alukg <guyaluk@gmail.com>
2016-05-03 20:07:53 +0300

    calc changes

--- src/MainComponents/Calculator.java
+++ src/MainComponents/Calculator.java
@@ -4,8 +4,6 @@ import UserInterface.CommandLineUI;
 import UserInterface.InputFileUI;
 import UserInterface.UI;
 
-import java.io.IOException;
-
 /**
  * The main class that runs the program.
  * Determines which UI will work.
@@ -14,10 +12,10 @@ public class Calculator {
     private static UI ui;
 
     public static void main(String[] args){
-        if(args.length>0){ //if txt file name inserted.
+        if(args.length>0){ //if text file name inserted.
             ui = new InputFileUI(args[0]); //creates an instance of the InputFileUI.
         }
-        else{ //if txt file name wasn't inserted.
+        else{ //if text file name wasn't inserted.
             ui = new CommandLineUI();
         }
         try {
